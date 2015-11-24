4082f55cf3b61b2389f2a3ecc8ee588f0d596f57
Marcelofrb <marcelofelipe11df@gmail.com>
2015-11-24 20:02:15 -0200

    Update and rename CDs.java to lCDs.java
    Código comentado para facilitar o entendimento do usuário

--- lCDs.java
+++ lCDs.java
@@ -1,15 +1,16 @@
 package ProjetoHerenca;
 
+//Classe feita com objetivo de instanciar um produto do tipo CD
 public class CDs extends Produto{
 
 	
 	private int num_Faixas = 0;
-	
+	//Metodo construtor
 	public CDs(int cod_Barras) {
 		super(cod_Barras);
 	}
 
-
+	//Método criado para coletar as caracteristicas do objeto
 	public void dados (String nome, double preco, int num_Faixas){
 		super.dados(nome, preco);
 		this.num_Faixas = num_Faixas;
